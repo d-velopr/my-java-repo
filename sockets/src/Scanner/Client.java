2356f64ff53971c884a811bb64fd84c0d9b1b891
vDev <134214060+d-velopr@users.noreply.github.com>
2025-03-18 21:07:25 -0700

    Update Client.java

--- sockets/src/Scanner/Client.java
+++ sockets/src/Scanner/Client.java
@@ -9,7 +9,7 @@ public class Client{
     public static void main(String[] args) {
         try {
  //           System.out.println("Client Started");
-            Socket soc = new Socket("192.168.10.122", 9806); // Establish client-to-server connection
+            Socket soc = new Socket("192.168.10.###", port#); // Establish client-to-server connection
 
             Scanner clientInput = new Scanner(System.in); // Establish Scanner for clients input
             System.out.println("What is your name?");    // Once connection and scanner are established, do this on clients computer
